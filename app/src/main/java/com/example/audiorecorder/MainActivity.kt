e0a4ff9b5cb428c641b499e13b65eefe01f73aaa
Kenneth Syddall <kensyd_98@hotmail.com>
2022-10-14 14:46:04 -0300

    Audio Recorder V2.

--- app/src/main/java/com/example/audiorecorder/MainActivity.kt
+++ app/src/main/java/com/example/audiorecorder/MainActivity.kt
@@ -1,11 +1,129 @@
 package com.example.audiorecorder
 
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import com.example.audiorecorder.databinding.ActivityMainBinding
+import java.io.IOException
+import java.sql.Time
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.jar.Manifest
 
-class MainActivity : AppCompatActivity() {
+//Grabadora de audio #4: Temporizador con controlador en Android Studio minuto 8
+
+
+const val REQUEST_CODE = 200
+class MainActivity : AppCompatActivity(), Timer.OnTimerTickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var permission = arrayOf(android.Manifest.permission.RECORD_AUDIO)
+    private var permissionGranted = false
+
+    private var recorder: MediaRecorder? = null
+    private var dirPath = ""
+    private var filename = ""
+    private var isRecording = false
+    private var isPaused = false
+
+    private lateinit var timer: Timer
+
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        permissionGranted = ActivityCompat.checkSelfPermission(this, permission[0]) == PackageManager.PERMISSION_GRANTED
+
+        if (!permissionGranted) {
+            ActivityCompat.requestPermissions(this, permission, REQUEST_CODE)
+        }
+
+        timer = Timer(this)
+
+        binding.btnRecord.setOnClickListener {
+            when{
+                isPaused -> resumeRecording()
+                isRecording -> pauseRecording()
+                else -> startRecording()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE)
+            permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun pauseRecording(){
+        recorder?.pause()
+        isPaused = true
+        binding.btnRecord.setImageResource(R.drawable.ic_record)
+
+        timer.pause()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun resumeRecording(){
+        recorder?.resume()
+        isPaused = false
+        binding.btnRecord.setImageResource(R.drawable.ic_pause)
+
+        timer.start()
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun startRecording(){
+        if(!permissionGranted){
+            ActivityCompat.requestPermissions(this, permission, REQUEST_CODE)
+            return
+        }
+
+        dirPath = "${externalCacheDir?.absolutePath}/"
+
+        var simpleDateFormat = SimpleDateFormat("yyyy.MM.DD_hh.mm.ss")
+        var date = simpleDateFormat.format(Date())
+        filename = "audio_record_$date"
+
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setOutputFile("$dirPath$filename.mp3")
+
+            try {
+                prepare()
+            }catch (e: IOException){}
+
+            start()
+        }
+
+        binding.btnRecord.setImageResource(R.drawable.ic_pause)
+        isRecording = true
+        isPaused = false
+
+        timer.start()
+    }
+
+    private fun stopRecording(){
+        timer.stop()
+    }
+
+    override fun onTimerTick(duration: String) {
+        println(duration)
     }
 }
